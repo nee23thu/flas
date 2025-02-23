9cae7ccd92fd7b275d78b15254df8a56cb6f2cfd
Neethuu <139195955+nee23thu@users.noreply.github.com>
2025-02-23 16:39:18 +0530

    modified config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Neethu@localhost:5432/user_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Neethu@localhost:5432/user_db_test"
-    TESTING = True
